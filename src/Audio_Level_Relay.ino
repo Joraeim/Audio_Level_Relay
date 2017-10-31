c2fc4a318171fc9b810d6484c2419e54964a4fb2
Jed Hobbs <jedhobbs@LenovoDebian>
2017-10-30 22:01:05 -0500

    Initial Read and write levels

--- src/Audio_Level_Relay.ino
+++ src/Audio_Level_Relay.ino
@@ -5,14 +5,34 @@
  * Date:
  */
 
+ int ledPin = D1;                // LED connected to digital pin D1
+ int analogPin = A0;             // potentiometer connected to analog pin A0
+ int val = 0;                    // variable to store the read value
+ int too_loud_count = 0;
+ int count_threshold = 100;
+ int level_threshold = 255;
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  pinMode(ledPin, OUTPUT);      // sets the ledPin as output
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+  val = analogRead(analogPin);
+  if(val > level_threshold)
+  {
+    too_loud_count++;
+    delay(100);
+  }
+
+  if(too_loud_count > count_threshold)
+  {
+    digitalWrite(ledPin, 1);
+    delay(5000);
+    too_loud_count = 0;
+    digitalWrite(ledPin, 0);
+  }
   // The core of your code will likely live here.
 
 }
